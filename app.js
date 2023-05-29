0055878ee66cc49bc3a0e44534643aa0521997e5
Piotr Łojko <80154448+S19514@users.noreply.github.com>
2023-05-29 23:25:19 +0200

    publish

---
+++ app.js
@@ -0,0 +1,136 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      battleLog: [],
+      playerHealth: 100,
+      monsterHealth: 100,
+      maxHealth: 100,
+      minHealth: 0,
+      currentRound: 0,
+      attackTimeout: 500,
+      winner: null,
+      battleLogVisible: false,
+    };
+  },
+  methods: {
+    attackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(5, 12);
+      if (this.monsterHealth - attackValue < this.minHealth) {
+        this.monsterHealth = this.minHealth;
+      } else {
+        this.battleLog.push('Player dealed ' + attackValue + ' damage');
+        this.monsterHealth -= attackValue;
+      }
+      setTimeout(() => {
+        this.attackPlayer();
+      }, this.attackTimeout);
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      if (this.playerHealth - attackValue < this.minHealth) {
+        this.playerHealth = this.minHealth;
+      } else {
+        this.battleLog.push('Monster dealed ' + attackValue + ' damage');
+        this.playerHealth -= attackValue;
+      }
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      if (this.monsterHealth - attackValue < this.minHealth) {
+        this.monsterHealth = this.minHealth;
+      } else {
+        this.battleLog.push('Player dealed ' + attackValue + ' damage');
+        this.monsterHealth -= attackValue;
+      }
+      setTimeout(() => {
+        this.attackPlayer();
+      }, this.attackTimeout);
+    },
+
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > this.maxHealth) {
+        this.playerHealth = this.maxHealth;
+      } else {
+        this.playerHealth += healValue;
+      }
+      setTimeout(() => {
+        this.attackPlayer();
+      }, this.attackTimeout);
+    },
+    surrender() {
+      this.winner = 'monster';
+    },
+    restartGame() {
+      this.playerHealth = this.maxHealth;
+      this.monsterHealth = this.maxHealth;
+      this.currentRound = 0;
+      this.winner = null;
+      this.battleLog = [];
+    },
+  },
+  computed: {
+    mosterBarStyles() {
+      return { width: this.monsterHealth + '%' };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + '%' };
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
+    playerHealthLevelStyles() {
+      if (this.playerHealth > 70) {
+        return { color: 'green' };
+      } else if (this.playerHealth > 30 && this.playerHealth <= 70) {
+        return { color: 'yellow' };
+      } else {
+        return { color: 'red' };
+      }
+    },
+    monsterHealthLevelStyles() {
+      if (this.monsterHealth > 70) {
+        return { color: 'green' };
+      } else if (this.monsterHealth > 30 && this.monsterHealth <= 70) {
+        return { color: 'yellow' };
+      } else {
+        return { color: 'red' };
+      }
+    },
+    battleLogText() {
+      if (this.battleLogVisible) {
+        return 'Hide Battle Log ↑ ';
+      } else {
+        return 'Show Battle Log ↓';
+      }
+    },
+  },
+  watch: {
+    monsterHealth(value) {
+      setTimeout(() => {
+        if (value === 0 && this.playerHealth === 0) {
+          this.winner = 'draw';
+        } else if (value === 0) {
+          this.winner = 'player';
+        }
+      }, this.attackTimeout);
+    },
+    playerHealth(value) {
+      setTimeout(() => {
+        if (value === 0 && this.monsterHealth === 0) {
+          this.winner = 'draw';
+        } else if (value === 0) {
+          this.winner = 'monster';
+        }
+      }, this.attackTimeout);
+    },
+  },
+});
+app.mount('#game');
